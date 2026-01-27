3fd413f3d6b3dea032f62b37b5b987dae3509339
Maitai <youremail@example.com>
2026-01-27 10:57:25 +0300

    Slight changes

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,17 +1,25 @@
-from pydantic_settings  import BaseSettings
-from dotenv import load_dotenv
-
-load_dotenv()
-
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
+    # Database and Security
     DATABASE_URL: str
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
-    ACCESS_TOKEN_EXPIRE_MINUTES: int = 4320
+    ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
+    
+    # Admin Credentials
+    ADMIN_EMAIL: str
+    ADMIN_PASSWORD: str
+    
+    # Cloudinary Settings
+    CLOUDINARY_NAME: str
+    CLOUDINARY_API_KEY: str
+    CLOUDINARY_API_SECRET: str
+    CLOUDINARY_URL: str 
 
     class Config:
         env_file = ".env"
+        extra = "ignore" 
+
 
 masettings = Settings()
-print(f"DEBUG: Secret Key loaded: {masettings.SECRET_KEY[:5]}***")
