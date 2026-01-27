b13025ed9a0c5d3f66ab47d1db557da92d29862f
Maitai <youremail@example.com>
2026-01-27 09:18:57 +0300

    Ngori

--- backend/main.py
+++ backend/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from app.database.connection import engine,Base
 from app.models import farmers, product,user
 from app.routes import farmers,product,auth
+from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
 
 
@@ -31,3 +32,15 @@ app.include_router(auth.router)
 @app.get("/")
 def home():
     return {"message": "Gwan to Maitai Dorper Farm API 🌾"}
+class ContactInquiry(BaseModel):
+    name: str
+    phone: str
+    interest: str
+    message: str
+
+@app.post("/api/contact")
+async def handle_contact(inquiry: ContactInquiry):
+    
+    print(f"New Inquiry from {inquiry.name} regarding {inquiry.interest}")
+    
+    return {"status": "success", "message": "Maitai Farm has received your inquiry!"}
